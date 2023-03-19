46510d54bc9d8f3d09995012d8d14e50da41e4e9
Ryan <ryedralisk@gmail.com>
2023-03-19 14:56:53 +0800

    feat: heappage doneded

--- src/java/simpledb/storage/HeapPage.java
+++ src/java/simpledb/storage/HeapPage.java
@@ -24,6 +24,8 @@ public class HeapPage implements Page {
     final byte[] header;
     final Tuple[] tuples;
     final int numSlots;
+    boolean dirty;
+    TransactionId dirtyTransactionId;
 
     byte[] oldData;
     private final Byte oldDataLock= (byte) 0;
@@ -52,7 +54,7 @@ public class HeapPage implements Page {
 
         // allocate and read the header slots of this page
         header = new byte[getHeaderSize()];
-        for (int i=0; i<header.length; i++)
+        for (int i=0; i < header.length; i++)
             header[i] = dis.readByte();
         
         tuples = new Tuple[numSlots];
@@ -79,7 +81,7 @@ public class HeapPage implements Page {
      * Retrieve maximum number of tuples this page can hold.
      */
     public int getMaxTuples() {
-        return (BufferPool.getPageSize()*8) / (td.getSize() * 8 + 1);
+        return (int) Math.floor(((double) BufferPool.getPageSize()*8) / (td.getSize() * 8 + 1));
     }
 
     /**
@@ -250,8 +252,18 @@ public class HeapPage implements Page {
      * @param t The tuple to delete
      */
     public void deleteTuple(Tuple t) throws DbException {
-        // some code goes here
-        // not necessary for lab1
+        if (t.getRecordId() == null)
+            throw new DbException("i dont even have a recordid what u doin");
+
+        int slotIndex = t.getRecordId().getTupleNumber();
+        if (!isSlotUsed(slotIndex))
+            throw new DbException("you cant delete an empty slot loser");
+        if (!t.getRecordId().getPageId().equals(this.getId()))
+            throw new DbException("this record isnt on this page");
+
+        // it is me kill me pls
+        markSlotUsed(slotIndex, false);
+        tuples[slotIndex].setRecordId(null);
     }
 
     /**
@@ -262,8 +274,25 @@ public class HeapPage implements Page {
      * @param t The tuple to add.
      */
     public void insertTuple(Tuple t) throws DbException {
-        // some code goes here
-        // not necessary for lab1
+        // Validation
+        if (this.getNumEmptySlots() == 0)
+            throw new DbException("no space for you loser");
+        if (!this.td.equals(t.getTupleDesc()))
+            throw new DbException("get outta here this tuple don't belong here");
+        
+        // add the tuple
+        // loop through the tuples until we find an empty slot to yeet t in
+        for (int i = 0; i < numSlots; i++) {
+            if (isSlotUsed(i))
+                continue;
+            // yeet him in
+            RecordId rid = new RecordId(pid, i);
+            tuples[i] = t;
+            tuples[i].setRecordId(rid);
+
+            markSlotUsed(i, true);
+            break;
+        }
     }
 
     /**
@@ -271,17 +300,17 @@ public class HeapPage implements Page {
      * that did the dirtying
      */
     public void markDirty(boolean dirty, TransactionId tid) {
-        // some code goes here
-	// not necessary for lab1
+        this.dirty = dirty;
+        this.dirtyTransactionId = tid;
     }
 
     /**
      * Returns the tid of the transaction that last dirtied this page, or null if the page is not dirty
      */
     public TransactionId isDirty() {
-        // some code goes here
-        // Not necessary for lab1
-        return null;      
+        if (dirty)
+            return dirtyTransactionId;
+        return null;
     }
 
     /**
@@ -308,15 +337,26 @@ public class HeapPage implements Page {
         }
     	byte byteSlot = header[byteAmt];
     	int bitmask = 1 << bitAmt;
-        return (byteSlot&bitmask) > 0;
+        return ((byteSlot&bitmask) >> bitAmt) == 1;
     }
 
     /**
      * Abstraction to fill or clear a slot on this page.
      */
     private void markSlotUsed(int i, boolean value) {
-        // some code goes here
-        // not necessary for lab1
+        int byteAmt = i / 8;
+        int bitAmt = i % 8;
+        if (byteAmt >= header.length || byteAmt < 0) {
+            return;
+        }
+        if (value) { // overwrite the bit as 1
+            int bitmask = 1 << bitAmt;
+            header[byteAmt] |= bitmask;
+        } else {     // overwrite the bit as 0
+            int bitmask = 1 << bitAmt;
+            header[byteAmt] |= bitmask; // set it to 1
+            header[byteAmt] ^= bitmask; // set it to 0 without affecting other bits
+        }
     }
 
     /**
