e531c8a1269365bd90701255e4d86c5ac3f7effd
Ryan <ryedralisk@gmail.com>
2023-04-08 17:44:08 +0800

    i like to touch my big lock

--- src/java/simpledb/storage/LockManager.java
+++ src/java/simpledb/storage/LockManager.java
@@ -9,89 +9,56 @@ import simpledb.transaction.TransactionAbortedException;
 import simpledb.transaction.TransactionId;
 
 public class LockManager {
-    private ConcurrentHashMap<PageId, StampedLock> pageLocks;
-    private ConcurrentHashMap<PageId, TransactionId> writeLockHolders;
-    private ConcurrentHashMap<PageId, List<TransactionId>> readLockHolders;
+    private ConcurrentHashMap<PageId, MyFirstLock> pageLocks;
 
     public LockManager() {
         this.pageLocks = new ConcurrentHashMap<>();
-        this.writeLockHolders = new ConcurrentHashMap<>();
-        this.readLockHolders = new ConcurrentHashMap<>();
     }
 
     private void ensurePageInitialised(PageId pid) {
         if (!pageLocks.containsKey(pid)) {
-            pageLocks.put(pid, new StampedLock());
-            readLockHolders.put(pid, new ArrayList<>());
-            // writeLockHolders.put(pid, null);
+            pageLocks.put(pid, new MyFirstLock());
         }
     }
 
-    private boolean hasOtherReaders(TransactionId tid, PageId pid) {
-        for (TransactionId otherTid : readLockHolders.get(pid)) {
-            if (!otherTid.equals(tid))
-                return true;
-        }
-        return false;
-    }
+    // private boolean hasOtherReaders(TransactionId tid, PageId pid) {
+    //     for (TransactionId otherTid : readLockHolders.get(pid)) {
+    //         if (!otherTid.equals(tid))
+    //             return true;
+    //     }
+    //     return false;
+    // }
 
     public boolean hasReadLock(TransactionId tid, PageId pid) {
         ensurePageInitialised(pid);
-        if (!readLockHolders.containsKey(pid))
-            return false;
-
-        return readLockHolders.get(pid).contains(tid);
+        return pageLocks.get(pid).holdsReadLock(tid);
     }
 
     public boolean hasWriteLock(TransactionId tid, PageId pid) {
         ensurePageInitialised(pid);
-        if (!writeLockHolders.containsKey(pid))
-            return false;
-        
-        TransactionId otherTid = writeLockHolders.get(pid);
-        return (!(otherTid == null) && otherTid.equals(tid));
+        return pageLocks.get(pid).holdsWriteLock(tid);
     }
 
     public void getReadLock(TransactionId tid, PageId pid) {
         ensurePageInitialised(pid);
-        if (hasReadLock(tid, pid))
-            return;
-
-        if (hasWriteLock(tid, pid)) // you already have a write lock idiot what you doing
-            return;
-
-        pageLocks.get(pid).asReadLock().lock();
-        readLockHolders.get(pid).add(tid);
+        pageLocks.get(pid).acquireReadLock(tid);
     }
 
     public void releaseReadLock(TransactionId tid, PageId pid) {
         ensurePageInitialised(pid);
-        readLockHolders.get(pid).remove(tid);
-        pageLocks.get(pid).asReadLock().unlock();
+        pageLocks.get(pid).releaseReadLock(tid);
     }
 
     public void getWriteLock(TransactionId tid, PageId pid) {
         ensurePageInitialised(pid);
-
-        if (hasWriteLock(tid, pid)) // you already have a write lock dumbass what you doin
-            return;
-        
-        if (hasReadLock(tid, pid)) { // you have a read lock
-            if (pageLocks.get(pid).tryConvertToWriteLock(0) != 0)
-                writeLockHolders.put(pid, tid);
-            return;
-        }
-
-        pageLocks.get(pid).asWriteLock().lock();
-        writeLockHolders.put(pid, tid);
+        pageLocks.get(pid).acquireWriteLock(tid);
     }
 
     public void releaseWriteLock(TransactionId tid, PageId pid) throws TransactionAbortedException{
         ensurePageInitialised(pid);
-        if (!writeLockHolders.get(pid).equals(tid)) // what the hell you're not the holder why do you have the write lock
+        if (!pageLocks.get(pid).holdsWriteLock(tid)) // what the hell you're not the holder why do you have the write lock
             throw new TransactionAbortedException(); //ABORT ABORT ABORT ABORT ABORT
-        writeLockHolders.remove(pid);
-        pageLocks.get(pid).asWriteLock().unlock();
+        pageLocks.get(pid).releaseWriteLock(tid);
     }
 
     /**
@@ -101,7 +68,6 @@ public class LockManager {
      */
     public void releaseWriteLockExceptionless(TransactionId tid, PageId pid){
         ensurePageInitialised(pid);
-        writeLockHolders.remove(pid);
-        pageLocks.get(pid).asWriteLock().unlock();
+        pageLocks.get(pid).releaseWriteLock(tid);
     }
 }
